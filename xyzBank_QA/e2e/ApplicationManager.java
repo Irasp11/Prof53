76cf7b7e8b84553a94214b4ee03546da525f5d96
Irasp11 <117228523+Irasp11@users.noreply.github.com>
2023-06-06 22:22:27 +0300

    Add files via upload

---
+++ xyzBank_QA/e2e/ApplicationManager.java
@@ -0,0 +1,26 @@
+package e2e;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    public WebDriver driver;
+
+    public void init() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    public void stop() {
+        driver.quit();
+    }
+
+}
+//  driver.close(); - закрывает текущую вкладку
+
---
+++ xyzBank_QA/pages/CustomerLoginPage.java
@@ -0,0 +1,27 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CustomerLoginPage extends PageBase {
+
+    public CustomerLoginPage(WebDriver driver) {
+        super(driver);
+
+    }
+
+    @FindBy(xpath = "//*[@ng-model='custId']")
+    WebElement customerNameDropDown;
+
+    @FindBy(xpath = "//*[@type='submit']")
+    WebElement loginButton;
+
+    public void selectCustomerName(String customerName) {
+        selectDropDownText(customerNameDropDown, customerName);
+    }
+
+    public void clickOnLoginButton() {
+        click(loginButton);
+    }
+}
