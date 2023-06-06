caa77c8c7bd404d15a32d2b0605c46ab1e044b42
Irasp11 <117228523+Irasp11@users.noreply.github.com>
2023-06-06 22:26:01 +0300

    Delete CustomersPage.java

--- xyzBank_QA/src/test/java/pages/CustomersPage.java
+++
@@ -1,9 +0,0 @@
-package pages;
-
-import org.openqa.selenium.WebDriver;
-
-public class CustomersPage extends BankManagerLoginPage {
-    public CustomersPage(WebDriver driver) {
-        super(driver);
-    }
-}
